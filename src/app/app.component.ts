7c8aa08bf0a42b1b4631548e721df25e8ad3395b
tahsinturag <tahsinturag10@gmail.com>
2024-11-21 13:42:26 +0600

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [StudentRegistrationComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'student-reg';
+
 }
+
+
